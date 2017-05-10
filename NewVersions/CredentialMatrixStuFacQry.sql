de3424c9173f051134c7db612b73c18348d99839
bobbylog <senghor.etienne@gmail.com>
2017-05-10 17:01:53 -0400

    qry051017
    qry051017

--- NewVersions/CredentialMatrixStuFacQry.sql
+++ NewVersions/CredentialMatrixStuFacQry.sql
@@ -1,15 +1,15 @@
 select StudentUID, StudentID, LastName, FirstName, typeid, AdmitDate from Student where 
---LastName like 'stief%'
+LastName like 'quin%'
 --and 
-StudentID='A0000015526'
+--StudentID='A0000015526'
 
 select [dbo].[getAdvisorDetailsByStudentID] ('sp-17','A0000015526',6) as advisordetails
 
-select FacultyID, LastName, FirstName from faculty where lastname like 'ried%'
+select FacultyID, LastName, FirstName from faculty where lastname like 'donahue%'
 
-select FacultyID, PortalAlias, PortalPassword, PortalAccess from FacultyPortal where FacultyID=53841
+select FacultyID, PortalAlias, PortalPassword, PortalAccess from FacultyPortal where FacultyID=110091
 
 select StudentUID, PortalPassword, LastName, FirstName, PortalHandle, PortalEnable, PortalPIN, PINValidateDT from CAMS_Student_Portal_Who_View
-where StudentUID=66272
+where StudentUID=49067
 
 --select * from Student where LastName='shannon'
