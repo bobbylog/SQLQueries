3622f58b18944e80aa5e9ec042fc2cf3711030ea
bobbylog <senghor.etienne@gmail.com>
2017-09-07 16:23:13 -0400

    chg090717_3
    chg090717_3

---
+++ NewVersions/StudentEMailPerTermQry1.sql
@@ -0,0 +1,14 @@
+SELECT DISTINCT 
+               TOP (100) PERCENT TC1.TextTerm AS Term, ST1.StudentID, LTRIM(RTRIM(ST1.LastName)) AS LastName, LTRIM(RTRIM(ST1.FirstName)) 
+               AS FirstName, ST1.MiddleInitial, MM1.MajorMinorName AS DegreeProgram, dbo.getStudentEmailAddressFromID(St1.StudentID) as Email1
+FROM  dbo.SRAcademic AS SR1 LEFT OUTER JOIN
+               dbo.TermCalendar AS TC1 ON SR1.TermCalendarID = TC1.TermCalendarID LEFT OUTER JOIN
+               dbo.Glossary AS GL1 ON SR1.CategoryID = GL1.UniqueId LEFT OUTER JOIN
+               dbo.Student AS ST1 ON SR1.StudentUID = ST1.StudentUID LEFT OUTER JOIN
+               dbo.StudentStatus AS SS1 ON SR1.StudentUID = SS1.StudentUID AND SR1.TermCalendarID = SS1.TermCalendarID LEFT OUTER JOIN
+               dbo.StudentProgram AS SP1 ON SS1.StudentStatusID = SP1.StudentStatusID LEFT OUTER JOIN
+               dbo.MajorMinor AS MM1 ON SP1.MajorProgramID = MM1.MajorMinorID
+WHERE (TC1.TextTerm = 'Fa-17') 
+--AND (NOT (GL1.DisplayText = 'Transfer'))
+ AND (NOT (ST1.LastName = 'Testperson'))
+ORDER BY LastName, FirstName
